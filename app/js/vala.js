3563c8832f799587acc0485f898142d91d9ea834
tuanpembual <andro.medh4@gmail.com>
2014-03-18 10:28:53 +0700

    merger angular to bootstrap

---
+++ app/js/vala.js
@@ -0,0 +1,10 @@
+function testing() 	{ alert(Apps.hello()); 
+}
+function mkdir() 		{ (Apps.dir());
+}
+function upd() 			{ (Apps.update());
+}
+function upg() 			{ (Apps.upgrade());
+}
+function save() 		{ (Apps.injek());
+}
